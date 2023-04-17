5751babdab4fd94178bd0879feace04fdf485ce5
Adithya <jhadith88@gmail.com>
2023-04-17 14:10:03 +0530

    end of py1

--- pytorch1.py
+++ pytorch1.py
@@ -1,4 +1,32 @@
 import torch 
 
 x=torch.empty((1))
-print(x)
+
+y = torch.rand(2,3)
+e = torch.rand(2,3)
+
+z = torch.zeros(2,2,2)
+
+t = torch.tensor([2.3,1.9])
+# print(y)
+# print(z)
+print(t)
+
+#addition
+add1 = y+e
+#subraction
+sub1 = y-e
+#multiplication
+mul1 = y*e
+#division
+div1 = y/e
+
+#is cuda available
+
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+    y = torch.ones_like(x, device=device)
+    x = x.to(device)
+    z = x+y
+    print(z)
+    print(z.to("cpu", torch.double))
