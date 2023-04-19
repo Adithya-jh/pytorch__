641ad6ef9b3640971f97594d738e21a9ff749f8a
Adithya <jhadith88@gmail.com>
2023-04-19 18:43:18 +0530

    updated nn using torch

--- nn_torch.py
+++ nn_torch.py
@@ -1,4 +1,4 @@
-import numpy as np 
+import torch
 
 # Compute every step manually
 
@@ -6,10 +6,10 @@ import numpy as np
 # f = w * x 
 
 # here : f = 2 * x
-X = np.array([1, 2, 3, 4], dtype=np.float32)
-Y = np.array([2, 4, 6, 8], dtype=np.float32)
+X = torch.tensor([1, 2, 3, 4], dtype=torch.float32)
+Y = torch.tensor([2, 4, 6, 8], dtype=torch.float32)
 
-w = 0.0
+w = torch.tensor(0.0, dtype=torch.float32,requires_grad=True)
 
 # model output
 def forward(x):
@@ -21,14 +21,14 @@ def loss(y, y_pred):
 
 # J = MSE = 1/N * (w*x - y)**2
 # dJ/dw = 1/N * 2x(w*x - y)
-def gradient(x, y, y_pred):
-    return np.mean(2*x*(y_pred - y))
+# def gradient(x, y, y_pred):
+#     return np.mean(2*x*(y_pred - y))
 
 print(f'Prediction before training: f(5) = {forward(5):.3f}')
 
 # Training
 learning_rate = 0.01
-n_iters = 20
+n_iters = 30
 
 for epoch in range(n_iters):
     # predict = forward pass
@@ -37,13 +37,20 @@ for epoch in range(n_iters):
     # loss
     l = loss(Y, y_pred)
     
-    # calculate gradients
-    dw = gradient(X, Y, y_pred)
+    # calculate gradients = backward pass
+    # dw = gradient(X, Y, y_pred)
+    l.backward() #dl/dw
 
     # update weights
-    w -= learning_rate * dw
+    with torch.no_grad():
+        w -= learning_rate * w.grad
+
+    # zero_gradients
+    w.grad.zero_()
+
+
 
     if epoch % 2 == 0:
         print(f'epoch {epoch+1}: w = {w:.3f}, loss = {l:.8f}')
      
-print(f'Prediction after training: f(5) = {forward(5):.3f}')
+print(f'Prediction after training: f(6) = {forward(6):.3f}')
